063bd76cae57d400a86f92a1fe3b510b7db62e9d
Mario Alejandro Sabala Encarnacion <mariosabala101106@gmail.com>
2026-06-13 01:54:28 -0400

    Dirigentes: Almost all the Back-end

---
+++ eVote360.Core/Interfaces/IDirigentePoliticoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eVote360.Core.Interfaces
+{
+    internal interface IDirigentePoliticoService
+    {
+    }
+}
---
+++ eVote360.Infrastructure/Migrations/20260613054819_AddDirigentesP.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eVote360.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDirigentesP : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DirigentesPoliticos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    PartidoPoliticoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DirigentesPoliticos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DirigentesPoliticos_PartidosPoliticos_PartidoPoliticoId",
+                        column: x => x.PartidoPoliticoId,
+                        principalTable: "PartidosPoliticos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DirigentesPoliticos_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirigentesPoliticos_PartidoPoliticoId",
+                table: "DirigentesPoliticos",
+                column: "PartidoPoliticoId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DirigentesPoliticos_UsuarioId",
+                table: "DirigentesPoliticos",
+                column: "UsuarioId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DirigentesPoliticos");
+        }
+    }
+}
