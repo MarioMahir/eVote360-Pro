9909705042238d84e39632c5683d3cf3262a5405
thr333zzy <wensinfree@gmail.com>
2026-06-16 14:48:34 -0500

    Implementa módulos de alianzas políticas y asignación de candidatos

---
+++ eVote360.Core/Entities/AlianzaPolitica.cs
@@ -0,0 +1,18 @@
+﻿namespace eVote360.Core.Entities;
+
+public class AlianzaPolitica
+{
+    public int Id { get; set; }
+
+    public int PartidoSolicitanteId { get; set; }
+
+    public PartidoPolitico PartidoSolicitante { get; set; } = null!;
+
+    public int PartidoAliadoId { get; set; }
+
+    public PartidoPolitico PartidoAliado { get; set; } = null!;
+
+    public DateTime FechaSolicitud { get; set; } = DateTime.Now;
+
+    public EstadoAlianza Estado { get; set; } = EstadoAlianza.Pendiente;
+}
---
+++ eVote360.Core/Entities/AsignacionCandidatoPuesto.cs
@@ -0,0 +1,22 @@
+﻿namespace eVote360.Core.Entities;
+
+public class AsignacionCandidatoPuesto
+{
+    public int Id { get; set; }
+
+    public int EleccionId { get; set; }
+
+    public Eleccion Eleccion { get; set; } = null!;
+
+    public int CandidatoId { get; set; }
+
+    public Candidato Candidato { get; set; } = null!;
+
+    public int PuestoElectivoId { get; set; }
+
+    public PuestoElectivo PuestoElectivo { get; set; } = null!;
+
+    public DateTime FechaAsignacion { get; set; } = DateTime.Now;
+
+    public bool Activo { get; set; } = true;
+}
